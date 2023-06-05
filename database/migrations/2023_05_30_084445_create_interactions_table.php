a31d81b6cf07ee14cb290d622dd012f360f99a96
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-06-05 12:48:23 +0000

    run pint

--- database/migrations/2023_05_30_084445_create_interactions_table.php
+++ database/migrations/2023_05_30_084445_create_interactions_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Enums\InteractionType;
 use App\Enums\InteractionStatus;
+use App\Enums\InteractionType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
