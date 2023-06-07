2c688ff1778cbd4cdb7eaa40f804618453b75f85
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-07 15:03:16 +0000

    ajout de la nav

---
+++ resources/js/Components/Bases/Auditor/Navigation.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+    <div class="btm-nav h-16 fixed bottom-0">
+        <button>
+            <span class="material-symbols-rounded text-3xl"> home </span>
+        </button>
+        <button class="active">
+            <span class="material-symbols-rounded text-3xl"> search </span>
+        </button>
+        <button>
+            <span class="material-symbols-rounded text-3xl">
+                smart_display
+            </span>
+        </button>
+        <button>
+            <span class="material-symbols-rounded text-3xl"> person </span>
+        </button>
+    </div>
+</template>
