11aeea17b3ba1e8976741db304d18a0719979656
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-07 17:11:37 +0000

    navigation, ajout icon et changement actif

--- resources/js/Pages/Auditor/Home.vue
+++ resources/js/Pages/Auditor/Home.vue
@@ -1,13 +1,13 @@
 <script setup>
 import Header from "@/Components/Bases/Auditor/Header.vue";
 import Navigation from "@/Components/Bases/Auditor/Navigation.vue";
-import Chat from "@/Components/Bases/Auditor/Chat.vue";
+//import Chat from "@/Components/Bases/Auditor/Chat.vue";
 </script>
 
 <template>
     <div class="bg-black text-base-100 relative">
         <Header />
-        <Chat />
+        <!--<Chat />-->
         <Navigation />
     </div>
 </template>
