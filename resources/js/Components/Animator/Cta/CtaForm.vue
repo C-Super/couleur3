ba1f0a43ec0009b836773ca1d2ffffb3c9b2e19b
cassandrefro <91882634+cassandrefro@users.noreply.github.com>
2023-06-08 14:38:25 +0000

    composant resultats qcm et sondage

--- resources/js/Components/Animator/Cta/CtaForm.vue
+++ resources/js/Components/Animator/Cta/CtaForm.vue
@@ -2,6 +2,7 @@
 import { reactive } from "vue";
 import BaseCard from "@/Components/Bases/BaseCard.vue";
 import BaseButton from "@/Components/Bases/BaseButton.vue";
+import BaseBarChart from "@/Components/Animator/Bases/BaseBarChart.vue";
 import TextInput from "@/Components/TextInput.vue";
 import TimeInput from "@/Components/TimeInput.vue";
 import InputGroup from "@/Components/InputGroup.vue";
@@ -21,6 +22,25 @@ const form = reactive({
     link: "",
     ended_at: "",
 });
+
+const answers = [
+    {
+        id: 1,
+        label: "Réponse 1",
+        value: 10,
+    },
+    {
+        id: 2,
+        label: "Réponse 2",
+        value: 5,
+    },
+
+    {
+        id: 3,
+        label: "Réponse 3",
+        value: 1,
+    },
+];
 </script>
 
 <template>
@@ -39,6 +59,7 @@ const form = reactive({
             <input-group id="ended_at" label="Durée d'interaction">
                 <time-input id="ended_at" v-model="form.ended_at"></time-input>
             </input-group>
+            <base-bar-chart :data="answers" correct="2"></base-bar-chart>
         </template>
         <template #actions>
             <div v-if="isCreating" class="flex flex-row gap-3">
