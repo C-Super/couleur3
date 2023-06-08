903c879195c1c1359192f21849aca1347e0a9511
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-08 20:24:50 +0000

    Recommandation

--- resources/js/Components/Bases/Auditor/Recommandation.vue
+++ resources/js/Components/Bases/Auditor/Recommandation.vue
@@ -9,7 +9,112 @@
                 alt="Image émission recommandation"
             />
             <p class="text-base font-light">
-                1er juin 2023 <span class="text-primary">- 56 minutes</span>
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
+            </p>
+        </div>
+
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[25px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
+            </p>
+        </div>
+
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[25px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
+            </p>
+        </div>
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[25px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
+            </p>
+        </div>
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[25px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
+            </p>
+        </div>
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[25px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023
+                <span
+                    class="text-primary"
+                    style="color: rgba(255, 255, 255, 0.25)"
+                >
+                    - 56 minutes
+                </span>
+
+                <br />
+                <span class="font-semibold text-2xl">Pili Pili</span>
             </p>
         </div>
     </div>
