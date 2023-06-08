1098bde5e3a620dd69d9699769a232f5ba7fd93a
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-08 14:40:32 +0000

    sauvegarde du projet

---
+++ resources/js/Components/Bases/Auditor/Recommandation.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+    <div class="bg-black text-base-100 px-3.5">
+        <div class="flex items-center">
+            <img
+                class="h-[80px] w-[80px] mt-[40px] mr-3"
+                src="images/recommandation.png"
+                alt="Image émission recommandation"
+            />
+            <p class="text-base font-light">
+                1er juin 2023 <span class="text-primary">- 56 minutes</span>
+            </p>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Auditor/Home.vue
+++ resources/js/Pages/Auditor/Home.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Header from "@/Components/Bases/Auditor/Header.vue";
 import Navigation from "@/Components/Bases/Auditor/Navigation.vue";
-import Chat from "@/Components/Bases/Auditor/Chat.vue";
+//import Chat from "@/Components/Bases/Auditor/Chat.vue";
+import PageAccueil from "@/Components/Bases/Auditor/PageAccueil.vue";
 </script>
 
 <template>
     <div class="bg-black text-base-100 relative">
         <Header />
-        <Chat />
+        <div style="height: calc(100vw * 9 / 16); background-color: aqua"></div>
         <Navigation />
+        <PageAccueil />
     </div>
 </template>
