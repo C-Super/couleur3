8affb8439054e9c8234bcc3e9c06b02aefa6909b
nicolaswunderle <91967460+nicolaswunderle@users.noreply.github.com>
2023-06-09 07:02:02 +0000

    Menu de navigation terminé

--- resources/js/Components/Bases/Auditor/Navigation.vue
+++ resources/js/Components/Bases/Auditor/Navigation.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 
+// permet de récupérer la personne authentifiée si elle existe
 defineProps({
     authInf: {
         type: Object,
@@ -38,13 +39,21 @@ function handleButtonClick(index) {
             :class="['bg-black', { active: button.active }]"
             @click="handleButtonClick(index)"
         >
-            <span class="material-symbols-rounded text-3xl">{{
-                button.name
-            }}</span>
-        </button>
-        <button v-if="authInf !== null">
             <span
-                class="bg-base-100 text-black font-medium h-9 w-9 flex items-center justify-center rounded-full"
+                v-if="button.name !== 'person'"
+                class="material-symbols-rounded text-3xl"
+            >
+                {{ button.name }}
+            </span>
+            <span
+                v-if="button.name === 'person' && authInf === null"
+                class="material-symbols-rounded text-3xl"
+            >
+                {{ button.name }}
+            </span>
+            <span
+                v-if="button.name === 'person' && authInf !== null"
+                class="bg-base-100 text-black font-bold h-9 w-9 flex items-center justify-center rounded-full"
             >
                 <span>{{ authInf.name[0] }}</span>
             </span>
