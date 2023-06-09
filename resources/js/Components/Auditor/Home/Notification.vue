af4afce79d2553a4ae26051cab9c8b91181699f8
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-09 09:48:32 +0000

    ajout de notification

---
+++ resources/js/Components/Auditor/Home/Notification.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+    <div class="chat chat-start">
+        <div class="chat-image avatar">
+            <div class="w-10 rounded-full bg-base-100">
+                <img src="images/Bulle-COULEUR3.svg" />
+            </div>
+        </div>
+        <div class="chat-bubble gradient-auditor">
+            <span class="text-primary">Interagir avec l’animateur</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.gradient-auditor::before {
+    background-color: #ff67ff;
+}
+</style>
