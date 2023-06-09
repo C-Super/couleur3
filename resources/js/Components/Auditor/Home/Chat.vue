e82ad04628071955c789a815e1e6a607faef2502
nicolaswunderle <91967460+nicolaswunderle@users.noreply.github.com>
2023-06-09 16:49:25 +0000

    Mise en page du chat

--- resources/js/Components/Auditor/Home/Chat.vue
+++ resources/js/Components/Auditor/Home/Chat.vue
@@ -1,13 +1,121 @@
-<script setup></script>
+<script setup>
+function check($event) {
+    if ($event.target.checked) {
+        const heightHeaderPlayer =
+            document.querySelector("#the-header").getBoundingClientRect()
+                .height +
+            document.querySelector("#player_container").getBoundingClientRect()
+                .height +
+            "px";
+        document
+            .querySelectorAll("body, #the-header, #player_container")
+            .forEach((element) => {
+                element.classList.add("chat-open");
+            });
+        document.querySelector("#fixed-container").classList.add("bg-black");
+        document.querySelector("#player_container").classList.add("top-16");
+        document.querySelector("#description-live").style.marginTop =
+            heightHeaderPlayer;
+        document.querySelector("#fixed-container").style.top =
+            heightHeaderPlayer;
+        document.querySelector("#chat-auditor").style.gridTemplateRows =
+            "auto 1fr";
+    } else {
+        document
+            .querySelectorAll("body, #the-header, #player_container")
+            .forEach((element) => {
+                element.classList.remove("chat-open");
+            });
+        document.querySelector("#fixed-container").classList.remove("bg-black");
+        document.querySelector("#player_container").classList.remove("top-16");
+        document.querySelector("#description-live").style.marginTop = "";
+        document.querySelector("#fixed-container").style.top = "";
+        document.querySelector("#chat-auditor").style.gridTemplateRows = "";
+    }
+}
+</script>
 
 <template>
-    <div class="collapse bg-blue-audior px-3.5">
-        <input type="checkbox" />
-        <div class="collapse-title text-xl font-medium">
-            Click me to show/hide content
+    <div
+        id="chat-auditor"
+        class="collapse bg-blue-audior rounded-t-[44px] rounded-b-none"
+    >
+        <input
+            type="checkbox"
+            class="w-full h-11 min-h-0"
+            @change="check($event)"
+        />
+        <div
+            class="collapse-title text-base font-extrabold h-11 min-h-0 p-0 flex items-center justify-center"
+        >
+            Chat<span
+                class="material-symbols-rounded ml-1 align-middle transition-transform"
+            >
+                expand_less
+            </span>
         </div>
-        <div class="collapse-content">
-            <p>hello</p>
+        <div class="collapse-content px-3.5 flex flex-col gap-y-3.5">
+            <div class="overflow-y-scroll h-2 grow flex flex-col-reverse">
+                <!-- Ajouter les nouveaux message ici-->
+                <p>hellodeb</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hellofin</p>
+            </div>
+            <div class="flex gap-x-5">
+                <input
+                    class="bg-blue-audior rounded-full border-base-100 focus:border-base-100 focus:outline-0 placeholder:font-extralight placeholder:text-base-100"
+                    type="text"
+                    placeholder="Message"
+                />
+                <button
+                    class="border border-base-100 flex items-center justify-center grow rounded-full"
+                >
+                    <span class="material-symbols-rounded align-middle">
+                        send
+                    </span>
+                </button>
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+input[type="checkbox"]:checked ~ div:first-of-type > span {
+    transform: rotate(180deg);
+}
+input[type="checkbox"]:checked ~ div:last-of-type {
+    padding-bottom: 0.875rem;
+}
+</style>
--- resources/js/Pages/Auditor/Index.vue
+++ resources/js/Pages/Auditor/Index.vue
@@ -4,7 +4,7 @@ import Player from "@/Components/Auditor/Player.vue";
 import DescriptionLive from "@/Components/Auditor/Home/DescriptionLive.vue";
 import ChatComponent from "@/Components/ChatComponent.vue";
 import Chat from "@/Components/Auditor/Home/Chat.vue";
-import Notification from "@/Components/Auditor/Home/Notification.vue";
+// import Notification from "@/Components/Auditor/Home/Notification.vue";
 
 defineProps({
     chatEnabled: {
@@ -23,9 +23,13 @@ defineProps({
         <Player />
         <DescriptionLive />
         <ChatComponent :chat-enabled="chatEnabled" />
-        <div class="fixed bottom-16 right-0 left-0">
-            <Notification />
-            <Chat />
+        <div
+            id="fixed-container"
+            class="fixed bottom-16 w-screen flex flex-col"
+        >
+            <!-- <Notification /> -->
+            <p>salut</p>
+            <Chat class="grow" />
         </div>
     </AuditorLayout>
 </template>
