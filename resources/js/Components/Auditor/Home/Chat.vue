db0306e883b73fcba9cfd64854b12f1d2664f09c
nicolaswunderle <91967460+nicolaswunderle@users.noreply.github.com>
2023-06-09 09:41:44 +0000

    Rajout des règles css et avancement dans le chat

--- resources/js/Components/Auditor/Home/Chat.vue
+++ resources/js/Components/Auditor/Home/Chat.vue
@@ -1,15 +1,13 @@
 <script setup></script>
 
 <template>
-    <div class="bg-blue-audior px-3.5">
-        <div class="collapse bg-base-200">
-            <input type="checkbox" />
-            <div class="collapse-title text-xl font-medium">
-                Click me to show/hide content
-            </div>
-            <div class="collapse-content">
-                <p>hello</p>
-            </div>
+    <div class="collapse bg-blue-audior px-3.5">
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium">
+            Click me to show/hide content
+        </div>
+        <div class="collapse-content">
+            <p>hello</p>
         </div>
     </div>
 </template>
