9ea8ede71b50a6c717f18c579ff01312d6c2615b
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-12 13:06:52 +0000

    Modification de notification et du popup

--- resources/js/Components/Auditor/Home/Notification.vue
+++ resources/js/Components/Auditor/Home/Notification.vue
@@ -1,7 +1,43 @@
-<script setup></script>
+<script setup>
+
+
+const props = defineProps({
+    interactionType: {
+        type: String,
+        required: true,
+    },
+});
+
+let notificationType = "";
+
+if (
+    props.interactionType === "TEXT" ||
+    props.interactionType === "MCQ" ||
+    props.interactionType === "SURVEY" ||
+    props.interactionType === "AUDIO" ||
+    props.interactionType === "VIDEO" ||
+    props.interactionType === "PICTURE"
+) {
+    notificationType = "normal";
+} else if (props.interactionType === "CTA") {
+    notificationType = "link";
+} else if (props.interactionType === "QUICK_CLICK") {
+    notificationType = "quick";
+}
+
+function clicNotif() {
+    alert("clic");
+}
+</script>
 
 <template>
-    <div id="notification-animateur" class="chat chat-start">
+    <div
+
+        id="notification-animateur"
+        class="chat chat-start"
+        data-interaction-type="interactionType"
+        @click="clicNotif()"
+    >
         <div class="chat-image avatar">
             <div class="w-10 rounded-full bg-base-100">
                 <img src="images/Bulle-COULEUR3.svg" />
@@ -12,15 +48,19 @@
         >
             Interagir avec l’animateur
             <div class="indicator absolute top-0 right-0 mt-1 mr-1">
-                <span
-                    class="indicator-item badge bg-[#7D7AFF] border-[#7D7AFF]"
-                ></span>
+                <span class="indicator-item badge bg-info border-info"></span>
                 <div class="grid w-32 h-32"></div>
             </div>
         </div>
     </div>
 
-    <div id="notification-lien" class="chat chat-start">
+    <div
+        v-if="notificationType === 'link'"
+        id="notification-lien"
+        class="chat chat-start"
+        data-interaction-type="interactionType"
+        @click="clicNotif()"
+    >
         <div class="chat-image avatar">
             <div class="w-10 rounded-full bg-base-100">
                 <img src="images/Bulle-COULEUR3.svg" />
@@ -31,20 +71,26 @@
         >
             <span class="mr-2"
                 >Texte de l’animateur
-                <span id="link" class="material-symbols-rounded align-middle text-xl	"
+                <span
+                    id="link"
+                    class="material-symbols-rounded align-middle text-xl"
                     >open_in_new</span
                 ></span
             >
 
             <div class="indicator absolute top-0 right-0 mt-1 mr-1">
-                <span
-                    class="indicator-item badge bg-[#7D7AFF] border-[#7D7AFF]"
-                ></span>
+                <span class="indicator-item badge bg-info border-info"></span>
                 <div class="grid w-32 h-32"></div>
             </div>
         </div>
     </div>
-    <div id="notification-rapide" class="chat chat-start text-black">
+    <div
+        v-if="notificationType === 'quick'"
+        id="notification-rapide"
+        class="chat chat-start text-black"
+        data-interaction-type="interactionType"
+        @click="clicNotif()"
+    >
         <div class="chat-image avatar">
             <div class="w-10 rounded-full bg-base-100">
                 <img src="images/Bulle-COULEUR3.svg" />
@@ -55,9 +101,7 @@
         >
             Sois le premier à cliquer !
             <div class="indicator absolute top-0 right-0 mt-1 mr-1">
-                <span
-                    class="indicator-item badge bg-[#7D7AFF] border-[#7D7AFF]"
-                ></span>
+                <span class="indicator-item badge bg-info border-info"></span>
                 <div class="grid w-32 h-32"></div>
             </div>
         </div>
---
+++ resources/js/Components/Auditor/Home/PopupText.vue
@@ -0,0 +1,9 @@
+<script setup>
+import BasePopup from "@/Components/Auditor/Bases/BasePopup.vue";
+</script>
+
+<template>
+    <BasePopup :text-boutton="Envoyer" :typeTitre="avis">
+        <input type="text" />
+    </BasePopup>
+</template>
