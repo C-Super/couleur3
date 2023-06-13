f2052d3b3f4dc0025d78c63200b74fba57725405
Daniel Mendes <contact@daniel-mendes.ch>
2023-06-13 22:08:44 +0000

    Fix lint

--- app/Http/Controllers/InteractionController.php
+++ app/Http/Controllers/InteractionController.php
@@ -8,63 +8,12 @@ use App\Http\Requests\Interaction\StoreCallToActionRequest;
 use App\Http\Requests\Interaction\StoreMCQRequest;
 use App\Http\Requests\Interaction\StoreQuickClickRequest;
 use App\Http\Requests\Interaction\StoreSurveyRequest;
-use App\Http\Requests\StoreInteractionRequest;
-use App\Jobs\CheckInteractionEnded;
 use App\Models\CallToAction;
 use App\Models\Interaction;
-use App\Models\Reward;
 use Auth;
-use DateTime;
-use Inertia\Inertia;
 
 class InteractionController extends Controller
 {
-    public function store(StoreInteractionRequest $request)
-    {
-        // Initialize $cta to null
-        $cta = null;
-
-        $validated = $request->validated();
-
-        // Then, depending on the type of interaction, create the call_to_action or question_choice
-        if ($request->type === 'survey' || $request->type === 'mcq') {
-
-            $interaction = Interaction::create($validated);
-            foreach ($validated['question_choice_data'] as $questionChoiceData) {
-                $interaction->question_choices()->create($questionChoiceData);
-            }
-            $interaction->load('question_choices');
-        } elseif ($request->type === 'cta' || $request->type === 'quick_click') {
-            foreach ($request->call_to_action_data as $ctaData) {
-                $cta = CallToAction::create($ctaData);
-            }
-            $interaction = Interaction::create(array_merge($validated, ['call_to_action_id' => $cta->id]));
-            $interaction->load('call_to_action');
-        } else {
-            $interaction = Interaction::create($validated);
-        }
-
-        if ($interaction->ended_at) {
-            $ended_at = new DateTime($interaction->ended_at);
-            $delay = $ended_at->getTimestamp() - now()->getTimestamp();
-
-            if ($delay > 0) {
-                CheckInteractionEnded::dispatch($interaction)->delay($delay);
-            }
-        }
-
-        $response = ['message' => 'Interaction created', 'interaction' => $interaction];
-
-        broadcast(new InteractionCreated($interaction))->toOthers();
-
-        $reward = Reward::all();
-
-        return Inertia::render('Animator/Interaction/Show', [
-            'interaction' => $interaction,
-            'rewards' => $reward,
-        ]);
-    }
-
     public function storeCTA(StoreCallToActionRequest $request)
     {
         $validated = $request->validated();
