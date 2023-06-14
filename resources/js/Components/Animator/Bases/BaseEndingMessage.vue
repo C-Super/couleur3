ff37f49d9d3beee429d5c102185940de708ffe32
CarinaFonseca <91950390+CarinaFonseca@users.noreply.github.com>
2023-06-14 09:31:17 +0000

    Affichage message de remerciement

---
+++ resources/js/Components/Animator/Bases/BaseEndingMessage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {useInteractionStore} from "@/Stores/useInteractionStore.js";
+import { storeToRefs } from "pinia";
+const interactionStore = useInteractionStore();
+const {winners} = storeToRefs(interactionStore);
+</script>
+<template>
+    <p>Merci! Un message a été envoyé aux gagnants suivants</p>
+    <ul class="flex flex-col gap-2">
+        <li
+            v-for="winner of winners"
+            :key="winner.value"
+            :for="winner.value" >
+            <div class="font-bold">
+                {{ winner.auditor.user.name }}
+            </div>
+        </li>
+        <li>
+            <p class="font-light">{{winners.length}} réponses</p>
+        </li>
+    </ul>
+</template>
