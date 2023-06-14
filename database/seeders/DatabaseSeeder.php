c1d4883fc3d2b56fb700818354fb6594f34f7550
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-06-14 13:54:35 +0000

    Ajout seeder reward

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AnimatorSeeder::class,
             AuditorSeeder::class,
+            RewardSeeder::class,
         ]);
     }
 }
