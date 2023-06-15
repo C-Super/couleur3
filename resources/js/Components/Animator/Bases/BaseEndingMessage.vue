dd03568a48e6dab26cbac356e2ca7d9b1c87649d
CarinaFonseca <91950390+CarinaFonseca@users.noreply.github.com>
2023-06-15 12:41:23 +0000

    affichage message de fin

--- resources/js/Components/Animator/Bases/BaseEndingMessage.vue
+++ resources/js/Components/Animator/Bases/BaseEndingMessage.vue
@@ -1,22 +1,29 @@
 <script setup>
-import {useInteractionStore} from "@/Stores/useInteractionStore.js";
+import BaseCard from "@/Components/Animator/Bases/BaseCard.vue";
+import Color from "@/Enums/Color";
+import { useInteractionStore } from "@/Stores/useInteractionStore.js";
 import { storeToRefs } from "pinia";
 const interactionStore = useInteractionStore();
-const {winners} = storeToRefs(interactionStore);
+const { winners } = storeToRefs(interactionStore);
 </script>
 <template>
-    <p>Merci! Un message a été envoyé aux gagnants suivants</p>
-    <ul class="flex flex-col gap-2">
-        <li
-            v-for="winner of winners"
-            :key="winner.value"
-            :for="winner.value" >
-            <div class="font-bold">
-                {{ winner.auditor.user.name }}
-            </div>
-        </li>
-        <li>
-            <p class="font-light">{{winners.length}} réponses</p>
-        </li>
-    </ul>
+    <base-card :color="Color.PRIMARY">
+        <template #title
+            >Merci! Un message a été envoyé aux gagnants suivant</template
+        >
+        <ul class="flex flex-col gap-2">
+            <li
+                v-for="winner of winners"
+                :key="winner.value"
+                :for="winner.value"
+            >
+                <div class="font-bold">
+                    {{ winner.auditor.user.name }}
+                </div>
+            </li>
+            <li>
+                <p class="font-light">{{ winners.length }} réponses</p>
+            </li>
+        </ul>
+    </base-card>
 </template>
