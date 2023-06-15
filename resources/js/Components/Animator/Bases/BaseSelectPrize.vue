cd7a2b36ad1e0022e9fb85014ba06544e9adfde4
CarinaFonseca <91950390+CarinaFonseca@users.noreply.github.com>
2023-06-15 08:13:00 +0000

    Custom option select v1

--- resources/js/Components/Animator/Bases/BaseSelectPrize.vue
+++ resources/js/Components/Animator/Bases/BaseSelectPrize.vue
@@ -10,19 +10,23 @@ console.log(prizes);
 </script>
 <template>
     <input-label> Lot remporté</input-label>
+
     <select
-        class="select select-bordered w-full max-w-xs rounded-full bg-transparent border-white focus:border-primary focus:ring-red"
+        class="select select-bordered w-full max-w-xs rounded-full bg-transparent border-white focus:border-primary font-light"
     >
         <option class="bg-neutral-200" disabled selected>
             Sélectionnez un lot
         </option>
         <option
             v-for="prize of prizes"
+            id="color"
             :key="prize.value"
+            :value="prize.name"
             :for="prize.value"
-            class="text-black bg-neutral-200"
+            class="text-black"
         >
             {{ prize.name }}
         </option>
     </select>
 </template>
+<style></style>
