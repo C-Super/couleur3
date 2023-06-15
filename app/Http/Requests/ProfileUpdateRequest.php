8ac9d2e205f8ca63017888c7b179082d05b4b577
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2023-06-15 21:07:14 +0000

    Ajout changement d'adresse

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,10 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'address.street' => 'sometimes|required_with_all:address.zip_code,address.city,address.country|string|max:255',
+            'address.zip_code' => 'sometimes|required_with_all:address.street,address.city,address.country|integer',
+            'address.city' => 'sometimes|required_with_all:address.street,address.zip_code,address.country|string|max:255',
+            'address.country' => 'sometimes|required_with_all:address.street,address.zip_code,address.city|string|max:255',
         ];
     }
 }
