bcd8172fe4e239e97ffb45ec47921c8a57a05302
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2023-06-15 15:07:02 +0000

    update profile

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -33,10 +33,8 @@ defineProps({
         </template>
 
         <div>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div
-                    class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
-                >
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 pt-32">
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
                     <UpdateProfileInformationForm
                         :must-verify-email="mustVerifyEmail"
                         :status="status"
@@ -44,15 +42,11 @@ defineProps({
                     />
                 </div>
 
-                <div
-                    class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
-                >
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
                     <UpdatePasswordForm class="max-w-xl" />
                 </div>
 
-                <div
-                    class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
-                >
+                <div class="p-4 sm:p-8 bg-black shadow sm:rounded-lg">
                     <DeleteUserForm class="max-w-xl" />
                 </div>
             </div>
