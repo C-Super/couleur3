fdb7ba20ecf4c4340ad73841a5d0b53d389b1063
nicolaswunderle <91967460+nicolaswunderle@users.noreply.github.com>
2023-06-15 16:12:44 +0000

    affichage du cadeau

--- resources/js/Components/Auditor/Home/Popups.vue
+++ resources/js/Components/Auditor/Home/Popups.vue
@@ -46,6 +46,12 @@ watch(hasOpenedNotif, () => {
 // Constante pour afficher ou non les titres et la validation
 const formValidation = ref(false);
 const popupTitle = computed(() => {
+    // Rajouter si l'utilisateur fait partie des réponse mettre thanks
+    // if (props.authInf !== null) {
+    //     if (currentInteraction.value.answers.find((user) => user.id === props.authInf.id)) {
+    //         return PopupTitleType.THANKS;
+    //     }
+    // }
     if (InteractionType.isQuestion(currentInteraction.value?.type)) {
         return PopupTitleType.QUESTION;
     } else if (currentInteraction.value?.type === InteractionType.QUICK_CLICK) {
@@ -120,7 +126,7 @@ function handleButtonPopup($event) {
                 <div class="flex justify-center mt-10">
                     <BaseButtonPopup
                         v-if="
-                            popupTitle === PopupTitleType.THANKS &&
+                            (popupTitle === PopupTitleType.GIFT || popupTitle === PopupTitleType.THANKS) &&
                             authInf !== null
                         "
                         id="close"
@@ -129,7 +135,7 @@ function handleButtonPopup($event) {
                         >Fermer</BaseButtonPopup
                     >
                     <BaseButtonPopup
-                        v-if="popupTitle === 'gift' && authInf === null"
+                        v-if="popupTitle === PopupTitleType.GIFT && authInf === null"
                         id="next"
                         :is-validate="true"
                         @click="handleButtonPopup"
@@ -137,6 +143,7 @@ function handleButtonPopup($event) {
                     >
                     <BaseButtonPopup
                         v-if="
+                            currentInteraction.type !== InteractionType.MCQ && currentInteraction.type !== InteractionType.SURVEY &&
                             popupTitle === PopupTitleType.QUESTION &&
                             authInf !== null
                         "
